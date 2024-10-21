782760273d41854e27e50ee0dcbc4b6006db30dc
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-21 13:26:16 +0200

    Add ColorPickerAnimation and ColorPircker components

---
+++ components/ColorPickerAnimation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ColorPickerAnimation = () => {
+  return (
+    <View>
+      <Text>ColorPickerAnimation</Text>
+    </View>
+  );
+};
+
+export default ColorPickerAnimation;
+
+const styles = StyleSheet.create({});
---
+++ components/ColorPircker.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ColorPircker = () => {
+  return (
+    <View>
+      <Text>ColorPircker</Text>
+    </View>
+  );
+};
+
+export default ColorPircker;
+
+const styles = StyleSheet.create({});
